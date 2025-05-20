35f207d205630617b483cbb803a039ee825850c0
Meghavina S B <111042012+meghavinasb@users.noreply.github.com>
2025-05-20 22:49:41 +0530

    Add files via upload

---
+++ plugins/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
+
+// Nuxt plugin setup
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.provide("auth", auth);
+  nuxtApp.provide("db", db);
+});
